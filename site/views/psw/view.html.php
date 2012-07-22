aaaeda0a3248ed2bd5ee069f6cc964431f8f9787
robertczech <robertczech@onet.eu>
2012-07-22 17:15:12 +0200

    wersja 1.0

---
+++ site/views/psw/view.html.php
@@ -0,0 +1,17 @@
+<?php
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.application.component.view');
+
+
+class PswViewPsw extends JView
+{
+	function display($tpl = null)
+	{
+		$psw = "psw";
+		$this->assignRef( 'psw', $psw );
+
+		parent::display($tpl);
+	}
+}
