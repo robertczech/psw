4991a00e55538e45909630c71aadede63b730eb6
robertczech <robertczech@onet.eu>
2012-07-23 16:54:26 +0200

    v1.1

---
+++ admin/tables/psw.php
@@ -0,0 +1,11 @@
+<?php defined('_JEXEC') or die('Restricted access');   
+class TablePsw extends JTable 
+{ 
+    var $id = null; 
+    var $psw = null; 
+    function __construct(&$db) 
+    { 
+        parent::__construct( '#__psw', 'id', $db); 
+     }
+}
+?>
